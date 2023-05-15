7c341e928458332c6dfb63ad63ac3df8e2b33237
nghinc <nghincpd05001@fpt.edu.vn>
2023-05-15 14:52:20 +0700

    init projrct

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
+import BarChart from './compoment/BarChart';
+import { UserData } from './Data'
+import LineChart from './compoment/LineChart';
+
+import PieChart from './compoment/PieChart';
+import ScatterChart from './compoment/ScatterChart';
+
 
 function App() {
+
+  const [userData, setUserData] = useState({
+    labels: UserData.map((data) => data.year),
+    datasets: [{
+      label: "User Gained",
+      data: UserData.map((data) => data.userGain),
+      backgroundColor: [
+        'rgba(75,192,192,1)',
+        '#ecf0f1',
+        '#50AF95',
+        '#f3ba2f',
+        '#2a71d10',
+      ],
+      borderColor: 'black',
+      borderWidth: 2,
+    },
+    ],
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ width: 700 }}>
+        <BarChart chartData={userData} />
+      </div>
+      <div style={{ width: 700 }}>
+        <LineChart chartData={userData} />
+      </div>
+      <div style={{ width: 700 }}>
+        <PieChart chartData={userData} />
+      </div>
+
+      {/* <ScatterChart /> */}
     </div>
   );
 }
---
+++ src/compoment/PieChart.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Doughnut } from 'react-chartjs-2'
+
+
+import { Chart as chartjs } from 'chart.js/auto'
+
+function PieChart({ chartData }) {
+    return <Doughnut data={chartData} />;
+}
+
+export default PieChart
